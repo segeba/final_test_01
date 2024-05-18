a415ce37add991fd962f812e3031e3dcafcf66b8
Sergey Bartasevich <sbartasevich@gmail.com>
2024-05-18 23:09:00 +0300

    Add comments to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,24 @@
-﻿string[] initArr()
+﻿//функция для ввода начального массива с клавиатуры
+string[] initArr()
 {
     int initArrLength;
     while (true)
     {
         Console.Write("Input array size: ");
         string input = Console.ReadLine();
+        // Попытка преобразовать введенную строку в целое число
+        // Если преобразование удалось и число больше нуля, выходим из цикла
         if (int.TryParse(input, out initArrLength) && initArrLength > 0) break;
+        // Если преобразование не удалось или число меньше/равно нулю, выводим сообщение об ошибке
         else Console.WriteLine("Invalid input. Please enter a positive number.");
     }
     string[] initArr = new string[initArrLength];
     for (int i = 0; i < initArr.Length; i++)
     {
+        //ввод элемента массива с клавиатуры
         System.Console.Write($"Input element {i + 1}: ");
         initArr[i] = Convert.ToString(Console.ReadLine());
+        //если не введено значение, то выводится повторный запрос на ввод элемента массива
         while (initArr[i] == "")
         {
             System.Console.Write($"Input element {i + 1}: ");
@@ -22,15 +28,18 @@
     return initArr;
 }
 
+//Формирования нового массива из начального массива согласно условию задачи
 string[] modArr(string[] initArr)
 {
     int modArrLength = 1;
+    //подсчет количества элементов массива, которые меньше 4 символов
     for (int i = 0; i < initArr.Length; i++)
     {
         if (initArr[i].Length < 4) modArrLength++;
     }
     string[] modArr = new string[modArrLength];
     int j = 0;
+    //заполнение нового массива элементами из исходного массива, которые меньше 4 символов
     for (int i = 0; i < initArr.Length; i++)
     {
         if (initArr[i].Length < 4)
@@ -42,6 +51,7 @@ string[] modArr(string[] initArr)
     return modArr;
 }
 
+//Вывод массива на экран
 void PrintArray(string[] Array)
 {
     for (int i = 0; i < Array.Length; i++)
@@ -50,11 +60,13 @@ void PrintArray(string[] Array)
     }
 }
 
+//вывод начального массива
 string[] array = initArr();
 System.Console.WriteLine("");
 System.Console.Write("Initial Array: ");
 PrintArray(array);
 
+//вывод измененного массива
 System.Console.WriteLine("");
 System.Console.WriteLine("");
 System.Console.Write("Overwrited Array: ");
